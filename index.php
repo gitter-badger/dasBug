05f28b518a9ff42ba111403571a666cd14261445
Paul McIntosh <pmcintosh@ingenium-ids.org>
2015-06-01 15:21:51 +0100

    Create a test array
    Created a test object

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 require_once ="dasBug.php";
+
+// test array
+$test=array(
+	'request'=>array(
+		'data'=>array(
+			'Client'=>array(
+				'id'=>1,
+				'firstname'=>'Steve',
+				'lastname'=>'Austins'
+			)
+		)
+	)
+);
+
 ?>
