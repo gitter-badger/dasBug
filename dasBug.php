1c4c7a6e038fc8692568d700e03b09a7689a1f89
Paul McIntosh <pmcintosh@ingenium-ids.org>
2015-06-01 14:56:59 +0100

    Initial commit
    Initial file commit

---
+++ dasBug.php
@@ -0,0 +1,39 @@
+<?php
+/**
+*
+**/
+class dasBug{
+
+	// constructor
+	public function __construct($module=false){
+
+		//TODO: Get line number, associated function, etcs
+
+
+		// get set module
+		if(isset($module) && !empty($module)){
+
+			// get type
+			$this->detect_type($module);
+			// end get type
+
+		}
+
+	}// end construct
+
+	// render
+	public function render($module=false){
+
+	}// end render
+
+	// detect type
+	public function detect_type($module=false){
+
+		// gettype()
+
+		//is_numeric, is_bool, is_null, is_float, is_int, is_string, is_object, is_array
+
+	}//end detect type
+
+}// end of dasBug
+?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require_once ="dasBug.php";
+?>
